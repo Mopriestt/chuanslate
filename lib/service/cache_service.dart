b48817aac45887c894630f6562ce934ffa3533b4
Yijun Li <Mopriestt@gmail.com>
2022-09-18 11:45:29 +1000

    Queue translations.

--- lib/service/cache_service.dart
+++ lib/service/cache_service.dart
@@ -1,12 +1,18 @@
+import 'dart:collection';
+
 import 'package:translator/translator.dart';
 
 class CacheService {
+  static const cacheLimit = 3000;
+
   final Map<String, Translation> _cache = {};
+  final Queue<String> _sources = Queue();
 
   Future<void> cacheWord(String source, Translation translation) async {
     _cache[source] = translation;
-    while (_cache.length > 3000) {
-      _cache.remove(_cache.keys.first);
+    _sources.add(source);
+    while (_cache.length > cacheLimit) {
+      _cache.remove(_sources.removeFirst());
     }
   }
 
